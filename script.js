485edf40316cb8ffd9bff07f82de57c40e167451
Thomas Jeriko <jerikothomas@gmail.com>
2018-04-13 22:51:07 +0700

    Menampilkan Soal

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
     }
 
     Question.prototype.displayQuestion = function() {
-        console.log(this.question);
+        document.getElementById('soal').innerHTML=this.question;
 
         for (var i = 0; i < this.answers.length; i++) {
             console.log(i + ': ' + this.answers[i]);
